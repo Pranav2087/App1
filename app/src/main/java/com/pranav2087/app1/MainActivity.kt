cb0648273e5441855327e1bfab7e000f7cfd985f
Pranav2087 <pranavprakash456@gmail.com>
2025-01-11 21:12:26 +0530

    changed the layout and implemented Glide for online image change

--- app/src/main/java/com/pranav2087/app1/MainActivity.kt
+++ app/src/main/java/com/pranav2087/app1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.pranav2087.app1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,8 +16,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binder.root)
         binder.button.setOnClickListener {
             binder.textView.text="Helloed World"
-            val newImageDrawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.image2)
-            binder.imageView.setImageDrawable(newImageDrawable)
+//            val newImageDrawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.image2)
+//            binder.imageView.setImageDrawable(newImageDrawable)
+            setImageUsingGlide()
         }
+    }
+    private fun setImageUsingGlide() {
+        Glide
+            .with(this)
+            .load("https://image.tmdb.org/t/p/original/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg")
+            .centerCrop()
+            .into(binder.imageView)
     }
 }
