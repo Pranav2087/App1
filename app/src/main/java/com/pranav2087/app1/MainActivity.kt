840de3e1167c6c29474bb66906a8fe901f0421ae
Pranav2087 <pranavprakash456@gmail.com>
2025-01-11 19:30:25 +0530

    Created a  small locally swappable image button and text

--- app/src/main/java/com/pranav2087/app1/MainActivity.kt
+++ app/src/main/java/com/pranav2087/app1/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.pranav2087.app1
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.pranav2087.app1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binder: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binder = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binder.root)
+        binder.button.setOnClickListener {
+            binder.textView.text="Helloed World"
+            val newImageDrawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.image2)
+            binder.imageView.setImageDrawable(newImageDrawable)
+        }
     }
 }
